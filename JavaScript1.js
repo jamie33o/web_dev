ad9eb9538512a009c4cebc353e0dd198dc5e839d
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-05-10 03:26:48 +0100

    all files

---
+++ JavaScript1.js
@@ -0,0 +1,8 @@
+﻿
+function blink() {
+    var elem = document.querySelector(".table");
+    elem.classList.toggle("hidden");
+}
+
+setInterval(blink, 500); // toggle the class every 500ms (half a second)
+
